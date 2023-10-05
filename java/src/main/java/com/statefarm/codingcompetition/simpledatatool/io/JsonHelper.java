95901bbbec99fef78bb6c3f4ad9c8bfa1d22b4be
Joseph Remy, Jr <josremy.dev@gmail.com>
2023-10-05 11:00:20 -0700

    feat(java): implement json loader, create unit test

--- java/src/main/java/com/statefarm/codingcompetition/simpledatatool/io/JsonHelper.java
+++ java/src/main/java/com/statefarm/codingcompetition/simpledatatool/io/JsonHelper.java
@@ -1,9 +1,12 @@
 package com.statefarm.codingcompetition.simpledatatool.io;
 
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class JsonHelper<T> {
 
@@ -11,7 +14,7 @@ public class JsonHelper<T> {
 
     private static Gson gson = new Gson();
 
-    public T[] loadJson(String fileName) {
+    public List<T> loadJson(String fileName, Class<T> clazz) {
 
         String filePathString = new StringBuilder(RESOURCE_FOLDER_PATH).append(fileName).toString();
         Path filePath = Path.of(filePathString);
@@ -19,7 +22,16 @@ public class JsonHelper<T> {
         try {
             String jsonAsString = new String(Files.readAllBytes(filePath));
 
+            Type typeOfT = TypeToken.getParameterized(List.class, clazz).getType();
+
+            List<T> javaModels = gson.fromJson(jsonAsString, typeOfT);
+
+            return javaModels;
+
         } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+
             return null;
         }
     }
--- java/src/test/java/com/statefarm/codingcompetition/simpledatatool/ApplicationTest.java
+++ java/src/test/java/com/statefarm/codingcompetition/simpledatatool/ApplicationTest.java
@@ -1,6 +1,39 @@
 package com.statefarm.codingcompetition.simpledatatool;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.statefarm.codingcompetition.simpledatatool.controller.SimpleDataTool;
+import com.statefarm.codingcompetition.simpledatatool.io.SimpleModel;
+
 public class ApplicationTest {
 
-    // TBD
+    private static SimpleDataTool controller;
+
+    @Before
+    public void initialize() {
+        controller = new SimpleDataTool();
+    }
+
+    // Example Test
+    @Test
+    public void readSimpleJson() {
+
+        List<SimpleModel> simpleModels = controller.loadSimpleModels();
+
+        assertNotNull("controller returned null", simpleModels);
+        assertEquals("controller did not return expected number of simple models", 1, simpleModels.size());
+
+        SimpleModel model1 = simpleModels.get(0);
+
+        assertEquals("John Smith", model1.getName());
+        assertEquals(1, model1.getInteger());
+        assertEquals(new BigDecimal("1.99"), model1.getDecimal());
+    }
 }
