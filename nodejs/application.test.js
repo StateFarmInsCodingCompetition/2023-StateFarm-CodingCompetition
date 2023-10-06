ba5b88da95e33739c68ffe930b142cfb99fc2784
Tanner Doe <massahos@gmail.com>
2023-10-06 10:32:16 -0700

    feat: adding node code

---
+++ nodejs/application.test.js
@@ -0,0 +1,13 @@
+const SimpleDataTool = require('./simpleDataTool');
+
+let controller;
+
+beforeEach(() => {
+    controller = new SimpleDataTool();
+});
+
+test('testLoadSimpleModels', async () => {
+    const data = await controller.loadSimpleModels();
+    expect(data).not.toBeNull();
+    // other assertions based on your test requirements...
+});
---
+++ nodejs/simpleDataTool.js
@@ -0,0 +1,21 @@
+const fs = require('fs').promises;
+
+class SimpleDataTool {
+    constructor() {
+        this.simpleModels = [];
+        this.JSON_FILENAME_SIMPLE = 'simple.json';
+    }
+
+    async loadSimpleModels() {
+        try {
+            const jsonData = await fs.readFile(this.JSON_FILENAME_SIMPLE, 'utf-8');
+            this.simpleModels = JSON.parse(jsonData);
+            return this.simpleModels;
+        } catch (err) {
+            console.error('Error loading JSON:', err);
+            return null;
+        }
+    }
+}
+
+module.exports = SimpleDataTool;
