4022a69a0090930d003a7dfa6551b8231deef498
Joseph Remy, Jr <josremy.dev@gmail.com>
2023-10-06 11:18:38 -0700

    update recommended extension lists

--- python/test/test_simple_data_tool.py
+++ python/test/test_simple_data_tool.py
@@ -1,18 +1,20 @@
 import pytest
 from simple_data_tool import SimpleDataTool
 
+
 @pytest.fixture
 def controller():
     return SimpleDataTool()
 
+
 def test_read(controller):
     simple_models = controller.load_simple_models()
 
-    assert(simple_models is not None)
-    assert(len(simple_models) == 1)
+    assert (simple_models is not None)
+    assert (len(simple_models) == 1)
 
     model_1 = simple_models[0]
 
-    assert(model_1.get("name") == "John Smith")
-    assert(model_1.get("integer") == 1)
-    assert(model_1.get("decimal") == 1.99)
+    assert (model_1.get("name") == "John Smith")
+    assert (model_1.get("integer") == 1)
+    assert (model_1.get("decimal") == 1.99)
