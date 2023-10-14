3ef13ffeee97a78ee08895761bc7193c4c582714
Ian Zhang <124932929+ianwzhang1@users.noreply.github.com>
2023-10-14 13:15:14 -0500

    Initialized for python project

--- java/src/main/java/com/statefarm/codingcompetition/simpledatatool/Application.java
+++
@@ -1,8 +0,0 @@
-package com.statefarm.codingcompetition.simpledatatool;
-
-public class Application {
-
-    public static void main(String[] args) {
-        System.out.println("I'm working");
-    }
-}
