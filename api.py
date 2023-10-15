501d4116d824c313be3a091ebc12d84e8896da06
Zachary Hampton <69336300+ZacharyHampton@users.noreply.github.com>
2023-10-14 21:34:26 -0700

    - spacing for api

--- api.py
+++ api.py
@@ -6,45 +6,57 @@ from models import Agents, Claims, Claim, ClaimHandlers, Disasters, Disaster, Di
 app = FastAPI(title="StateFarm Round 1")
 controller = SimpleDataTool()
 
+
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
 
 @app.get("/agents", response_model=Agents)
 def read_agents():
     agents = controller.get_agent_data()
     return Agents(agents=agents)
 
+
 @app.get("/claims", response_model=Claims)
 def read_claims():
     claims = controller.get_claim_data()
     return Claims(claims=claims)
 
+
 @app.get("/claims/{claim_id}", response_model=Claim)
 def read_claim(claim_id: int):
     claims = controller.get_claim_data()
+
     for claim in claims:
         if claim["id"] == claim_id:
             return Claim(**claim)
+
     raise HTTPException(status_code=404, detail=f"Claim with ID {claim_id} not found")
+
 
 @app.get("/claim-handlers", response_model=ClaimHandlers)
 def read_claim_handlers():
     claim_handlers = controller.get_claim_handler_data()
     return ClaimHandlers(claim_handlers=claim_handlers)
 
+
 @app.get("/disasters", response_model=Disasters)
 def read_disasters():
     disasters = controller.get_disaster_data()
     return Disasters(disasters=disasters)
 
+
 @app.get("/disasters/{disaster_id}", response_model=Disaster)
 def read_disaster(disaster_id: int):
     disasters = controller.get_disaster_data()
+
     for disaster in disasters:
         if disaster["id"] == disaster_id:
             return Disaster(**disaster)
+
     raise HTTPException(status_code=404, detail="Disaster not found")
+
 
 @app.get("/disasters/state/{state}", response_model=DisasterCount)
 async def get_disasters_for_state(state: str):
