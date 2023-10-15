954c5f199947a6b95bfe0686ebf4ba5b6b985e0c
Cullen Watson <cullen@cullenwatson.com>
2023-10-14 22:04:01 -0500

    test cases

--- simple_data_tool.py
+++ simple_data_tool.py
@@ -4,35 +4,33 @@ import pandas as pd
 from statistics import mean
 
 
-
 class SimpleDataTool:
-
-    AGENTS_FILEPATH = 'data/sfcc_2023_agents.json'
-    CLAIM_HANDLERS_FILEPATH = 'data/sfcc_2023_claim_handlers.json'
-    CLAIMS_FILEPATH = 'data/sfcc_2023_claims.json'
-    DISASTERS_FILEPATH = 'data/sfcc_2023_disasters.json'
+    AGENTS_FILEPATH = "data/sfcc_2023_agents.json"
+    CLAIM_HANDLERS_FILEPATH = "data/sfcc_2023_claim_handlers.json"
+    CLAIMS_FILEPATH = "data/sfcc_2023_claims.json"
+    DISASTERS_FILEPATH = "data/sfcc_2023_disasters.json"
 
     REGION_MAP = {
-        'west': 'Alaska,Hawaii,Washington,Oregon,California,Montana,Idaho,Wyoming,Nevada,Utah,Colorado,Arizona,New Mexico',
-        'midwest': 'North Dakota,South Dakota,Minnesota,Wisconsin,Michigan,Nebraska,Iowa,Illinois,Indiana,Ohio,Missouri,Kansas',
-        'south': 'Oklahoma,Texas,Arkansas,Louisiana,Kentucky,Tennessee,Mississippi,Alabama,West Virginia,Virginia,North Carolina,South Carolina,Georgia,Florida',
-        'northeast': 'Maryland,Delaware,District of Columbia,Pennsylvania,New York,New Jersey,Connecticut,Massachusetts,Vermont,New Hampshire,Rhode Island,Maine'
+        "west": "Alaska,Hawaii,Washington,Oregon,California,Montana,Idaho,Wyoming,Nevada,Utah,Colorado,Arizona,New Mexico",
+        "midwest": "North Dakota,South Dakota,Minnesota,Wisconsin,Michigan,Nebraska,Iowa,Illinois,Indiana,Ohio,Missouri,Kansas",
+        "south": "Oklahoma,Texas,Arkansas,Louisiana,Kentucky,Tennessee,Mississippi,Alabama,West Virginia,Virginia,North Carolina,South Carolina,Georgia,Florida",
+        "northeast": "Maryland,Delaware,District of Columbia,Pennsylvania,New York,New Jersey,Connecticut,Massachusetts,Vermont,New Hampshire,Rhode Island,Maine",
     }
 
     def __init__(self):
         self.__agent_data = self.load_json_from_file(self.AGENTS_FILEPATH)
         self.__claim_handler_data = self.load_json_from_file(
-            self.CLAIM_HANDLERS_FILEPATH)
+            self.CLAIM_HANDLERS_FILEPATH
+        )
         self.__claim_data = self.load_json_from_file(self.CLAIMS_FILEPATH)
-        self.__disaster_data = self.load_json_from_file(
-            self.DISASTERS_FILEPATH)
+        self.__disaster_data = self.load_json_from_file(self.DISASTERS_FILEPATH)
 
     # Helper Methods
 
     def load_json_from_file(self, filename):
         data = None
 
-        with open(filename, 'r', encoding='utf-8') as file:
+        with open(filename, "r", encoding="utf-8") as file:
             data = json.load(file)
 
         return data
@@ -62,7 +60,7 @@ class SimpleDataTool:
         data = self.get_claim_data()
         df = pd.DataFrame(data)
 
-        return len(df[df['status'] == 'Closed'])
+        return len(df[df["status"] == "Closed"])
 
     def get_num_claims_for_claim_handler_id(self, claim_handler_id):
         """Calculates the number of claims assigned to a specific claim handler
@@ -73,7 +71,12 @@ class SimpleDataTool:
         Returns:
             int: number of claims assigned to claim handler
         """
-        pass
+        data = self.get_claim_data()
+        df = pd.DataFrame(data)
+
+        handler_df = df[df["claim_handler_assigned_id"] == claim_handler_id]
+
+        return len(handler_df)
 
     def get_num_disasters_for_state(self, state):  #: z
         """Calculates the number of disasters for a specific state
@@ -101,8 +104,17 @@ class SimpleDataTool:
             float | None: estimate cost of disaster, rounded to the nearest hundredths place
                           returns None if no claims are found
         """
+        data = self.get_claim_data()
+        df = pd.DataFrame(data)
 
-        pass
+        filtered_df = df[df["disaster_id"] == disaster_id]
+
+        if len(filtered_df) == 0:
+            return None
+
+        total_cost = round(filtered_df["estimate_cost"].sum(), 2)
+
+        return total_cost
 
     def get_average_claim_cost_for_claim_handler(self, claim_handler_id):  #: z
         """Gets the average estimated cost of all claims assigned to a claim handler
@@ -125,12 +137,21 @@ class SimpleDataTool:
 
         Example: Say New Jersey and Delaware both have the highest number of disasters at
                  12 disasters each. Then, this method would return "Delaware" since "D"
-                 comes before "N" in the alphabet. 
+                 comes before "N" in the alphabet.
 
         Returns:
             string: single name of state
         """
-        pass
+        data = self.get_disaster_data()
+        df = pd.DataFrame(data)
+
+        state_counts = df.groupby("state").agg({"id": "nunique"}).reset_index()
+
+        state_counts = state_counts.sort_values(
+            by=["id", "state"], ascending=[False, True]
+        )
+
+        return state_counts.iloc[0]["state"]
 
     def get_state_with_least_disasters(self):  #: z
         """Returns the name of the state with the least disasters based on disaster data
@@ -140,13 +161,22 @@ class SimpleDataTool:
 
         Example: Say New Mexico and West Virginia both have the least number of disasters at
                  1 disaster each. Then, this method would return "New Mexico" since "N"
-                 comes before "W" in the alphabet. 
+                 comes before "W" in the alphabet.
 
         Returns:
             string: single name of state
         """
-        pass
-    
+        data = self.get_disaster_data()
+        df = pd.DataFrame(data)
+
+        state_counts = df.groupby("state").agg({"id": "nunique"}).reset_index()
+
+        state_counts = state_counts.sort_values(
+            by=["id", "state"], ascending=[True, True]
+        )
+
+        return state_counts.iloc[0]["state"]
+
     def get_most_spoken_agent_language_by_state(self, state):
         """Returns the name of the most spoken language by agents (besides English) for a specific state
 
@@ -157,13 +187,31 @@ class SimpleDataTool:
             string: name of language
                     or empty string if state doesn't exist
         """
-        pass
+        data = self.get_agent_data()
+        df = pd.DataFrame(data)
 
-    def get_num_of_open_claims_for_agent_and_severity(self, agent_id, min_severity_rating):  #: z
+        state_agents = df[
+            (df["state"] == state)
+            & (df["secondary_language"].notnull())
+            & (df["secondary_language"] != "English")
+        ]
+
+        if state_agents.empty:
+            return ""
+
+        most_common_language = (
+            state_agents["secondary_language"].value_counts().idxmax()
+        )
+
+        return most_common_language
+
+    def get_num_of_open_claims_for_agent_and_severity(
+        self, agent_id, min_severity_rating
+    ):  #: z
         """Returns the number of open claims for a specific agent and for a minimum severity level and higher
 
         Note: Severity rating scale for claims is 1 to 10, inclusive.
-        
+
         Args:
             agent_id (int): ID of the agent
             min_severity_rating (int): minimum claim severity rating
@@ -186,8 +234,12 @@ class SimpleDataTool:
         Returns:
             int: number of disasters where the declared date is after the end date
         """
+        data = self.get_disaster_data()
+        df = pd.DataFrame(data)
 
-        pass
+        late_declaration = df[df["declared_date"] > df["end_date"]]
+
+        return len(late_declaration)
 
     def build_map_of_agents_to_total_claim_cost(self):  #: z
         """Builds a map of agent and their total claim cost
@@ -217,7 +269,25 @@ class SimpleDataTool:
             float: density of claims to disaster area, rounded to the nearest thousandths place
                    None if disaster does not exist
         """
-        pass
+        disaster_data = self.get_disaster_data()
+        df_disaster = pd.DataFrame(disaster_data)
+
+        claim_data = self.get_claim_data()
+        df_claim = pd.DataFrame(claim_data)
+
+        disaster_row = df_disaster[df_disaster["id"] == disaster_id]
+
+        if disaster_row.empty:
+            return None
+
+        num_claims = df_claim[df_claim["disaster_id"] == disaster_id].shape[0]
+
+        radius = disaster_row.iloc[0]["radius_miles"]
+        area = math.pi * (radius**2)
+
+        density = num_claims / area
+
+        return round(density, 5)
 
     # endregion
 
--- test/test_simple_data_tool.py
+++ test/test_simple_data_tool.py
@@ -14,10 +14,10 @@ def test_read_data_files(controller):
     Args:
         controller (SimpleDataTool): utility script that loads JSON files and does comparisons
     """
-    assert (len(controller.get_agent_data()) == 100)
-    assert (len(controller.get_claim_handler_data()) == 156)
-    assert (len(controller.get_claim_data()) == 1000)
-    assert (len(controller.get_disaster_data()) == 100)
+    assert len(controller.get_agent_data()) == 100
+    assert len(controller.get_claim_handler_data()) == 156
+    assert len(controller.get_claim_data()) == 1000
+    assert len(controller.get_disaster_data()) == 100
 
 
 class TestSetOne:
@@ -34,73 +34,63 @@ class TestSetOne:
 
     def test_get_num_disasters_for_state(self, controller):
         """Test 3"""
-        assert controller.get_num_disasters_for_state(
-            'Arizona') == 2
-        assert controller.get_num_disasters_for_state(
-            'Georgia') == 5
-        assert controller.get_num_disasters_for_state(
-            'Illinois') == 2
-        assert controller.get_num_disasters_for_state(
-            'Texas') == 9
-        assert controller.get_num_disasters_for_state(
-            'District of Columbia') == 2
+        assert controller.get_num_disasters_for_state("Arizona") == 2
+        assert controller.get_num_disasters_for_state("Georgia") == 5
+        assert controller.get_num_disasters_for_state("Illinois") == 2
+        assert controller.get_num_disasters_for_state("Texas") == 9
+        assert controller.get_num_disasters_for_state("District of Columbia") == 2
 
 
 class TestSetTwo:
-
     def test_get_total_claim_cost_for_disaster(self, controller):
         """Test 4"""
         assert controller.get_total_claim_cost_for_disaster(5) == 377726.38
         assert controller.get_total_claim_cost_for_disaster(0) == None
-        assert controller.get_total_claim_cost_for_disaster(
-            56) == 1287476.19
+        assert controller.get_total_claim_cost_for_disaster(56) == 1287476.19
         assert controller.get_total_claim_cost_for_disaster(101) == None
-        assert controller.get_total_claim_cost_for_disaster(
-            78) == 614822.68
+        assert controller.get_total_claim_cost_for_disaster(78) == 614822.68
 
     def test_get_average_claim_cost_for_claim_handler(self, controller):
         """Test 5"""
-        assert controller.get_average_claim_cost_for_claim_handler(
-            2) == 87330.89
-        assert round(
-            controller.get_average_claim_cost_for_claim_handler(42), 2) == 122195.90
+        assert controller.get_average_claim_cost_for_claim_handler(2) == 87330.89
+        assert (
+            round(controller.get_average_claim_cost_for_claim_handler(42), 2)
+            == 122195.90
+        )
         assert controller.get_average_claim_cost_for_claim_handler(-5) == None
         assert controller.get_average_claim_cost_for_claim_handler(225) == None
-        assert round(
-            controller.get_average_claim_cost_for_claim_handler(151), 2) == 242134.96
+        assert (
+            round(controller.get_average_claim_cost_for_claim_handler(151), 2)
+            == 242134.96
+        )
 
     def test_get_state_with_most_and_least_disasters(self, controller):
         """Test 6"""
-        assert controller.get_state_with_most_disasters() == 'California'
-        assert controller.get_state_with_least_disasters() == 'Alaska'
+        assert controller.get_state_with_most_disasters() == "California"
+        assert controller.get_state_with_least_disasters() == "Alaska"
 
     def test_get_most_spoken_agent_language_by_state(self, controller):
         """Test 7"""
-        assert controller.get_most_spoken_agent_language_by_state(
-            'New Hampshire') == 'Arabic'
-        assert controller.get_most_spoken_agent_language_by_state(
-            'Wisconsin') == ''
-        assert controller.get_most_spoken_agent_language_by_state(
-            'Florida') == 'Spanish'
+        assert (
+            controller.get_most_spoken_agent_language_by_state("New Hampshire")
+            == "Arabic"
+        )
+        assert controller.get_most_spoken_agent_language_by_state("Wisconsin") == ""
+        assert (
+            controller.get_most_spoken_agent_language_by_state("Florida") == "Spanish"
+        )
 
     def test_get_num_of_open_claims_for_agent_and_severity(self, controller):
         """Test 8"""
-        assert controller.get_num_of_open_claims_for_agent_and_severity(
-            0, 0) == -1
-        assert controller.get_num_of_open_claims_for_agent_and_severity(
-            25, 11) == -1
-        assert controller.get_num_of_open_claims_for_agent_and_severity(
-            65, 3) == None
-        assert controller.get_num_of_open_claims_for_agent_and_severity(
-            24, 1) == 16
-        assert controller.get_num_of_open_claims_for_agent_and_severity(
-            87, 6) == 3
-        assert controller.get_num_of_open_claims_for_agent_and_severity(
-            85, 6) == 2
+        assert controller.get_num_of_open_claims_for_agent_and_severity(0, 0) == -1
+        assert controller.get_num_of_open_claims_for_agent_and_severity(25, 11) == -1
+        assert controller.get_num_of_open_claims_for_agent_and_severity(65, 3) == None
+        assert controller.get_num_of_open_claims_for_agent_and_severity(24, 1) == 16
+        assert controller.get_num_of_open_claims_for_agent_and_severity(87, 6) == 3
+        assert controller.get_num_of_open_claims_for_agent_and_severity(85, 6) == 2
 
 
 class TestSetThree:
-
     def test_get_num_disasters_declared_after_end_date(self, controller):
         """Test 9"""
         assert controller.get_num_disasters_declared_after_end_date() == 8
@@ -108,7 +98,7 @@ class TestSetThree:
     def test_build_map_of_agents_to_total_claim_cost(self, controller):
         """Test 10"""
         agent_cost_map = controller.build_map_of_agents_to_total_claim_cost()
-        assert len(agent_cost_map.keys()) == 100, 'Missing agents in map'
+        assert len(agent_cost_map.keys()) == 100, "Missing agents in map"
 
         # Normal cases
         assert agent_cost_map.get(1) == 27856.13
@@ -118,13 +108,44 @@ class TestSetThree:
         assert agent_cost_map.get(13) == 2310862.86
 
         # Spot-check random agent ids that we expect to have no cost
-        expected_agent_ids_without_cost = [2, 6, 9, 12, 16, 22, 25, 32, 33, 37, 38, 40,
-                                           41, 44, 45, 48, 50, 51, 52, 53, 54, 61, 64,
-                                           65, 67, 69, 72, 81, 90, 93, 96]
+        expected_agent_ids_without_cost = [
+            2,
+            6,
+            9,
+            12,
+            16,
+            22,
+            25,
+            32,
+            33,
+            37,
+            38,
+            40,
+            41,
+            44,
+            45,
+            48,
+            50,
+            51,
+            52,
+            53,
+            54,
+            61,
+            64,
+            65,
+            67,
+            69,
+            72,
+            81,
+            90,
+            93,
+            96,
+        ]
         num_agent_ids_without_cost = len(expected_agent_ids_without_cost)
         for i in range(3):
-            random_agent_id = expected_agent_ids_without_cost[random.randint(
-                0, num_agent_ids_without_cost - 1)]
+            random_agent_id = expected_agent_ids_without_cost[
+                random.randint(0, num_agent_ids_without_cost - 1)
+            ]
             assert agent_cost_map.get(random_agent_id) == 0
 
         # Testing invalid agent ids
@@ -140,11 +161,12 @@ class TestSetThree:
 
 
 class TestSetFour:
-
     def test_get_top_three_months_with_highest_num_of_claims_desc(self, controller):
         """Test 12"""
-        top_three_months = controller.get_top_three_months_with_highest_num_of_claims_desc()
+        top_three_months = (
+            controller.get_top_three_months_with_highest_num_of_claims_desc()
+        )
         assert len(top_three_months) == 3
-        assert top_three_months[0] == 'April 2023'
-        assert top_three_months[1] == 'November 2022'
-        assert top_three_months[2] == 'February 2023'
+        assert top_three_months[0] == "April 2023"
+        assert top_three_months[1] == "November 2022"
+        assert top_three_months[2] == "February 2023"
