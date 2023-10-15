780e3da07a171a63d5d6067d8257c42a916f4e50
Suharta <suharta098@gmail.com>
2023-10-15 00:20:54 -0400

    Test0, Test1 completed

--- java/src/main/java/com/statefarm/codingcompetition/simpledatatool/controller/SimpleDataTool.java
+++ java/src/main/java/com/statefarm/codingcompetition/simpledatatool/controller/SimpleDataTool.java
@@ -1,7 +1,7 @@
 package com.statefarm.codingcompetition.simpledatatool.controller;
 
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 
 import com.statefarm.codingcompetition.simpledatatool.io.JsonHelper;
 import com.statefarm.codingcompetition.simpledatatool.model.Agent;
@@ -58,28 +58,40 @@ public class SimpleDataTool {
      * @return number of closed claims
      */
     public int getNumClosedClaims() {
-        return 0;
+        int numClosedClaims = 0;
+        for(Claim claim : claims)
+            if("Closed".equals(claim.getStatus()))
+                numClosedClaims++;
+        return numClosedClaims;
     }
 
     /**
      * Calculates the number of claims assigned to a specific claim handler
-     * 
+     *
      * @param id id of claim handler
      * @return number of claims assigned to claim handler
      */
     public int getNumClaimsForClaimHandlerId(int id) {
-        return 0;
+        int numOfClaims=0;
+        for(Claim claim : claims)
+            if(id == claim.getClaim_handler_assigned_id())
+                numOfClaims++;
+        return numOfClaims;
     }
 
     /**
      * Calculates the number of disasters for a specific state
-     * 
+     *
      * @param stateName name of a state in the United States of America,
      *                  including the District of Columbia
      * @return number of disasters for state
      */
     public int getNumDisastersForState(String stateName) {
-        return -1;
+        int disastersOfState = 0;
+        for(Disaster disaster : disasters)
+            if(stateName.equals(disaster.getState()))
+                disastersOfState++;
+        return disastersOfState;
     }
 
     // endregion
@@ -88,76 +100,144 @@ public class SimpleDataTool {
 
     /**
      * Sums the estimated cost of a specific disaster by its claims
-     * 
+     *
      * @param id id of disaster
      * @return estimate cost of disaster, rounded to the nearest hundredths place
      *         returns null if no claims are found
      */
     public Float getTotalClaimCostForDisaster(int id) {
-        return -0.01f;
+        double sumOfCost = 0;
+        for(Claim claim : claims) {
+            if (id == claim.getDisaster_id())
+                sumOfCost += claim.getEstimate_cost();
+        }
+        float x = (float)sumOfCost;
+        if(x == 0)
+            return null;
+        return x;
     }
-
     /**
      * Gets the average estimated cost of all claims assigned to a claim handler
-     * 
+     *
      * @param id id of claim handler
      * @return average cost of claims, rounded to the nearest hundredths place,
      *         or null if no claims are found
      */
     public Float getAverageClaimCostforClaimHandler(int id) {
-        return -0.01f;
+        float sumOfAllClaims = 0;
+        float avg = 0;
+        int allClaims = getNumClaimsForClaimHandlerId(id);
+        if(allClaims == 0)
+            return null;
+        for(Claim claim : claims)
+            if(id == claim.getClaim_handler_assigned_id())
+                sumOfAllClaims+=claim.getEstimate_cost();
+        avg = sumOfAllClaims/allClaims;
+        avg = Math.round(avg*100) / 100.0f;
+        return avg;
     }
 
     /**
      * Returns the name of the state with the most disasters based on disaster data
-     * 
+     *
      * If two states have the same number of disasters, then sort by alphabetical
      * (a-z) and take the first.
-     * 
+     *
      * Example: Say New Jersey and Delaware both have the highest number of
      * disasters at 12 disasters each. Then, this method would return "Delaware"
      * since "D"comes before "N" in the alphabet.
-     * 
+     *
      * @return single name of state
      */
     public String getStateWithTheMostDisasters() {
-        return null;
+        Map<String, Integer> stateDisasterCount = new HashMap<>();
+        for(Disaster disaster : disasters) {
+            String state = disaster.getState();
+            stateDisasterCount.put(state,stateDisasterCount.getOrDefault(state,0)+1);
+        }
+        int max = 0;
+        List<String> equalStates = new ArrayList<>();
+        for(String state :stateDisasterCount.keySet()) {
+            int disasterCount = stateDisasterCount.get(state);
+            if(disasterCount > max) {
+                max = disasterCount;
+                equalStates.clear();
+                equalStates.add(state);
+            } else if (disasterCount == max)
+                equalStates.add(state);
+        }
+        Collections.sort(equalStates);
+        if(!equalStates.isEmpty())
+            return equalStates.get(0);
+        else return null;
     }
 
     /**
      * Returns the name of the state with the least disasters based on disaster data
-     * 
+     *
      * If two states have the same number of disasters, then sort by alphabetical
      * (a-z) and take the first.
-     * 
+     *
      * Example: Say New Mexico and West Virginia both have the least number of
      * disasters at 1 disaster each. Then, this method would return "New Mexico"
      * since "N" comes before "W" in the alphabet.
-     * 
+     *
      * @return single name of state
      */
     public String getStateWithTheLeastDisasters() {
-        return null;
+        Map<String, Integer> stateDisasterCount = new HashMap<>();
+        for(Disaster disaster : disasters) {
+            String state = disaster.getState();
+            stateDisasterCount.put(state,stateDisasterCount.getOrDefault(state,0)+1);
+        }
+        int min = Integer.MAX_VALUE;
+        List<String> equalStates = new ArrayList<>();
+        for(String state :stateDisasterCount.keySet()) {
+            int disasterCount = stateDisasterCount.get(state);
+            if(disasterCount < min) {
+                min = disasterCount;
+                equalStates.clear();
+                equalStates.add(state);
+            } else if (disasterCount == min)
+                equalStates.add(state);
+        }
+        Collections.sort(equalStates);
+        if(!equalStates.isEmpty())
+            return equalStates.get(0);
+        else return null;
     }
 
     /**
      * Returns the name of the most spoken language by agents (besides English) for
      * a specific state
-     * 
+     *
      * @param string name of state
      * @return name of language
      *         or empty string if state doesn't exist
      */
     public String getMostSpokenAgentLanguageByState(String string) {
-        return null;
+        Map<String, Integer> biLingual = new HashMap<>();
+        for(Agent agent : agents) {
+            if(string.equals(agent.getState()))
+                biLingual.put(agent.getSecondary_language(), biLingual.getOrDefault(agent.getSecondary_language(), 0)+1);
+        }
+        int max = 0;
+        String popLanguage = "";
+        for(String language :biLingual.keySet()) {
+            int popular = biLingual.get(language);
+            if (popular > max)
+                max = popular;
+            popLanguage = language;
+        }
+        return popLanguage;
     }
 
     /**
      * Returns the number of open claims for a specific agent and for a minimum
      * severity level and higher
-     * 
+     *
      * Note: Severity rating scale for claims is 1 to 10, inclusive.
-     * 
+     *
      * @param agentId           id of agent
      * @param minSeverityRating minimum claim severity rating
      * @return number of claims that are not closed and have minimum severity rating
@@ -166,7 +246,19 @@ public class SimpleDataTool {
      *         null if agent does not exist, or agent has no claims (open or not)
      */
     public Integer getNumOfOpenClaimsForAgentAndSeverity(int agentId, int minSeverityRating) {
-        return -2;
+        int openClaims = 0;
+        if(minSeverityRating < 1 || minSeverityRating > 10)
+            return -1;
+        for(Claim claim : claims) {
+            if (agentId == claim.getAgent_assigned_id() && minSeverityRating <= claim.getSeverity_rating()) {
+                if (!claim.getStatus().equals("Closed"))
+                    openClaims++;
+            }
+        }
+        if(openClaims > 0)
+            return openClaims;
+        else
+            return null;
     }
 
     // endregion
@@ -179,7 +271,14 @@ public class SimpleDataTool {
      * @return number of disasters where the declared date is after the end date
      */
     public int getNumDisastersDeclaredAfterEndDate() {
-        return -1;
+        int count = 0;
+        for(Disaster disaster : disasters) {
+            LocalDate declared = disaster.getDeclared_date();
+            LocalDate end = disaster.getEnd_date();
+            if(declared.isAfter(end))
+                count++;
+        }
+        return count;
     }
 
     /**
@@ -194,7 +293,14 @@ public class SimpleDataTool {
      *         to the agent
      */
     public Map<Integer, Float> buildMapOfAgentsToTotalClaimCost() {
-        return null;
+        Map<Integer,Float> agentsToCost = new HashMap<>();
+        for(Claim claim : claims) {
+            if (!agentsToCost.containsKey(claim.getAgent_assigned_id()))
+                agentsToCost.put(claim.getAgent_assigned_id(), claim.getEstimate_cost());
+            else if (agentsToCost.containsKey(claim.getAgent_assigned_id()))
+                agentsToCost.replace(claim.getAgent_assigned_id(), agentsToCost.getOrDefault(claim.getAgent_assigned_id(), 0f) + claim.getEstimate_cost());
+        }
+        return agentsToCost;
     }
 
     /**
