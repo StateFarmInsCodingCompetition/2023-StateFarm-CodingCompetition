065650edad9eeb3a2a40566f927ada9e814d5110
Cullen Watson <cullen@cullenwatson.com>
2023-10-14 23:30:02 -0500

    [fix] add models for api

--- api.py
+++ api.py
@@ -1,40 +1,58 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from simple_data_tool import SimpleDataTool
+from models import Agents, Claims, Claim, ClaimHandlers, Disasters, Disaster, DisasterCount
 
 app = FastAPI(title="StateFarm Round 1")
 controller = SimpleDataTool()
-
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
-
-@app.get("/agents")
+@app.get("/agents", response_model=Agents)
 def read_agents():
-    return controller.get_agent_data()
+    agents = controller.get_agent_data()
+    return Agents(agents=agents)
 
+@app.get("/claims", response_model=Claims)
+def read_claims():
+    claims = controller.get_claim_data()
+    return Claims(claims=claims)
 
-@app.get("/claim-handlers")
+@app.get("/claims/{claim_id}", response_model=Claim)
+def read_claim(claim_id: int):
+    claims = controller.get_claim_data()
+    for claim in claims:
+        if claim["id"] == claim_id:
+            return Claim(**claim)
+    raise HTTPException(status_code=404, detail=f"Claim with ID {claim_id} not found")
+
+@app.get("/claim-handlers", response_model=ClaimHandlers)
 def read_claim_handlers():
-    return controller.get_claim_handler_data()
+    claim_handlers = controller.get_claim_handler_data()
+    return ClaimHandlers(claim_handlers=claim_handlers)
 
-
-@app.get("/disasters")
+@app.get("/disasters", response_model=Disasters)
 def read_disasters():
-    return controller.get_disaster_data()
+    disasters = controller.get_disaster_data()
+    return Disasters(disasters=disasters)
 
-
-@app.get("/disasters/{disaster_id}")
+@app.get("/disasters/{disaster_id}", response_model=Disaster)
 def read_disaster(disaster_id: int):
     disasters = controller.get_disaster_data()
     for disaster in disasters:
         if disaster["id"] == disaster_id:
-            return disaster
-    return {"error": "Disaster not found"}
+            return Disaster(**disaster)
+    raise HTTPException(status_code=404, detail="Disaster not found")
 
+@app.get("/disasters/state/{state}", response_model=DisasterCount)
+async def get_disasters_for_state(state: str):
+    try:
+        num_disasters = controller.get_num_disasters_for_state(state)
+        return DisasterCount(state=state, number_of_disasters=num_disasters)
+    except ValueError:
+        raise HTTPException(status_code=404, detail=f"No data available for state: {state}")
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8010)
---
+++ models.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+
+class Agent(BaseModel):
+    id: int
+    name: str
+
+class Claim(BaseModel):
+    id: int
+    title: str
+
+class ClaimHandler(BaseModel):
+    id: int
+    name: str
+
+class Disaster(BaseModel):
+    id: int
+    type: str
+
+class DisasterCount(BaseModel):
+    state: str
+    number_of_disasters: int
+
+class Agents(BaseModel):
+    agents: list[Agent]
+
+class Claims(BaseModel):
+    claims: list[Claim]
+
+class ClaimHandlers(BaseModel):
+    claim_handlers: list[ClaimHandler]
+
+class Disasters(BaseModel):
+    disasters: list[Disaster]
