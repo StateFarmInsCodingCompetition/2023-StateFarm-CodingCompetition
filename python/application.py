eca920063d080af0e10a574b16de78f483db3685
Kyungchan Im (Chris) <ikc6751@gmail.com>
2023-10-14 18:04:13 -0700

    Update application.py

--- python/application.py
+++ python/application.py
@@ -1,1 +1,7 @@
-print("I'm working")
+from simple_data_tool import SimpleDataTool
+# Don't modify test_simple_data_tool.py 
+from test.test_simple_data_tool import TestSetOne, TestSetTwo, TestSetThree, TestSetFour
+
+
+dataset = SimpleDataTool()
+print(dataset.get_agent_data())
