63227816995d7993bc64b801c4cd59b6e0c276d3
Preston Roesslet <proesslet@gmail.com>
2023-10-14 22:14:23 -0500

    removed unnecessary imports

--- java/src/main/java/com/statefarm/codingcompetition/RestEntryPoint.java
+++ java/src/main/java/com/statefarm/codingcompetition/RestEntryPoint.java
@@ -1,12 +1,7 @@
 package com.statefarm.codingcompetition;
 
-
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.statefarm.codingcompetition.simpledatatool.controller.SimpleDataTool;
