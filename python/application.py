634467a1e382928441491d111396b93b4e637f33
Kyungchan Im (Chris) <ikc6751@gmail.com>
2023-10-14 20:02:49 -0700

    update

--- python/application.py
+++ python/application.py
@@ -1,7 +1,4 @@
 from simple_data_tool import SimpleDataTool
-# Don't modify test_simple_data_tool.py 
-from test.test_simple_data_tool import TestSetOne, TestSetTwo, TestSetThree, TestSetFour
 
 dataset = SimpleDataTool()
 
-print(dataset.get_regional_disaster_map())
